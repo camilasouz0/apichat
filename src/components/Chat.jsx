2f4c0e12e01a2b6c35a62f4e228276a4bb0669d7
camilasouz0 <camilamaiara@hotmail.com.br>
2021-03-27 00:53:29 -0300

    Ajuste nos metodos do yup e nos components do formik

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
-import balaofala from "../img/balao.png";
 import './style.css';
 
 const SignupSchema = Yup.object().shape({
   nome: Yup.string()
-    .min(100, <div id="posicao"><img className="balao-fala" id="image"src={balaofala} /><label className="texto-container" id="texto">Que satisfação, (nome). Agora que sei seu nome, qual cidade e estado você mora?</label></div>)
+    .min(100, <div className="texto-container"><label>Que satisfação, (nome). Agora que sei seu nome, qual cidade e estado você mora?</label></div>)
     .max(50, 'Too Long!')
     .required('Required'),
   cidade: Yup.string()
-    .min(100, <div id="posicao"><img className="balao-fala" id="image"src={balaofala} /><label className="texto-container" id="texto">Legal, agora sabemos sua cidade e estado. Quando foi que você nasceu?</label></div>)
+    .min(100, <div className="texto-container"><label>Legal, agora sabemos sua cidade e estado. Quando foi que você nasceu?</label></div>)
     .max(50, 'Too Long!')
     .required('Required'),
-  email: Yup.string()
-  .min(100, 'Qual o seu e-mail?')
-  .max(50, 'Too Long!')
-  .required('Required'),
+  date: Yup.date().default(function () {return new Date();})
+    .max(100, <div className="texto-container"><label>Qual o seu e-mail?</label></div>)
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required')/* .label(['start','pending','closed',]) */
 });
 
 export const ValidationSchemaExample = () => (
@@ -26,30 +25,39 @@ export const ValidationSchemaExample = () => (
       initialValues={{
         nome: '',
         cidade: '',
+        date: '',
         email: '',
       }}
       validationSchema={SignupSchema}
       onSubmit={values => {
-        // same shape as initial values
         console.log(values);
       }}
     >
-      {({ errors, touched }) => (
+    {({ errors, touched }) => (
         <div className="container-background">
           <div className="container-back-form">
             <Form className="container-scroll">
               <Field className="input-color" name="nome" placeholder="Nome e sobrenome"/>
               {errors.nome && touched.nome ? ( <div>{errors.nome}</div> ) : null}
-              {/* disabled={!errors.nome} */}
-              {errors.nome && touched.nome ? <Field className="input-color" name="cidade" placeholder="Cidade"/> : null}
+
+              {errors.nome && touched.nome ? <Field className="input-color" name="cidade" placeholder="Cidade" /> : null}
               
               { errors.cidade && touched.cidade ? ( <div>{errors.cidade}</div> ) : null}
 
-              { errors.cidade && touched.cidade ? <Field className="input-color" name="email" type={"number"} /> : null}
+              { errors.cidade && touched.cidade ? <Field className="input-color" name="date" type="date" /> : null}
+              
+              {errors.date && touched.date ? <div>{errors.date}</div> : null}
+
+              { errors.date && touched.date ? <Field className="input-color" name="email" type="email" /> : null}
               
               {errors.email && touched.email ? <div>{errors.email}</div> : null}
-              <button type="submit">Enviar</button>             
-            </Form>           
+
+              {errors.nome && touched.nome && !errors.email && touched.email ? <div className="texto-container"><label>Fim</label></div>  : null}
+
+              <button type="submit">Enviar</button> 
+                            
+            </Form>
+                    
           </div>
         </div>
       )}
